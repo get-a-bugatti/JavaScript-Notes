f28a1168f9c7cb39351f3ad19db14a3ba01966f0
get-a-bugatti <37701415+get-a-bugatti@users.noreply.github.com>
2024-08-08 21:54:44 +0545

    Some modifications and updates

--- PROJECT3/scripts3.js
+++ PROJECT3/scripts3.js
@@ -1,17 +1,6 @@
 const plusButton = document.querySelectorAll('div.plus');
 
-plusButton.forEach(button => {
-    button.addEventListener('click', function() {
-        const itemToReveal = button.classList[1];  // 2nd ClassName pertaining to identifier number
-
-        console.log(itemToReveal);
-        const itemClass = `div.answer.${itemToReveal}`;
-
-        const ToReveal = document.querySelector(itemClass);
-
-        ToReveal.style.display = 'block';
-    }) 
-}) // Made a mistake here. DO NOT FORGET TO LOOP ~~~
+// Made a mistake here. DO NOT FORGET TO LOOP ~~~
 
 
 
